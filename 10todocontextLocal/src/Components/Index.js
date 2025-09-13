5f41094795fdaa4474f4ca549289fe294c776c47
gitabhicode <yadavabhishek29200@gmail.com>
2025-09-13 20:38:50 +0530

    Todo app

---
+++ 10todocontextLocal/src/Components/Index.js
@@ -0,0 +1,4 @@
+import TodoForm from "./todoForm";
+import TodoItem from "./Todoitem";
+
+export {TodoForm, TodoItem}
---
+++ 10todocontextLocal/src/contexts/index.js
@@ -0,0 +1,4 @@
+// use of index.js file is to gather all the imports and export in other files
+
+export {TodoContext, TodoProvider, useTodo} from"./TodoContext"
+
